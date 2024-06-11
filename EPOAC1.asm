53f7387af305a76db7d771c599e59cdc8a03ee8a
Davi Batista <davidedeus28@hotmail.com>
2024-06-10 22:02:49 -0300

    Criando o código em alto nível

---
+++ EPOAC1.asm
@@ -0,0 +1,8 @@
+.text
+.globl main
+
+
+
+
+.data 
+$
---
+++ EPOAC1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void somabit(int b1, int b2, int* vaium, int* soma) {
+  *soma = (b1 ^ b2) ^ *vaium;
+  *vaium = (b1 & b2) | (b1 & *vaium) | (b2 & *vaium);
+}
+
+int main() {
+  int b1 = 0;
+  int b2 = 0;
+  int vaium = 0;
+  int soma;
+
+  somabit(b1, b2, &vaium, &soma);
+
+  printf("Soma: %d\n", soma);
+  printf("Vai-um: %d\n", vaium);
+
+  return 0;
+}
